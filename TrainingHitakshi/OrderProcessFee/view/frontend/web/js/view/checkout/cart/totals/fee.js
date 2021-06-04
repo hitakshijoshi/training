5bdf2ec367d98b4580e2b1cecd66652f443edea1
hitakshijoshi <hitakshi.joshi@krishtechnolabs.com>
2021-06-04 11:56:48 +0530

    Order Process Fee Module

---
+++ TrainingHitakshi/OrderProcessFee/view/frontend/web/js/view/checkout/cart/totals/fee.js
@@ -0,0 +1,18 @@
+define(
+    [
+        'TrainingHitakshi_OrderProcessFee/js/view/checkout/summary/fee'
+    ],
+    function (Component) {
+        'use strict';
+
+        return Component.extend({
+
+            /**
+             * @override
+             */
+            isDisplayed: function () {
+                return true;
+            }
+        });
+    }
+);
